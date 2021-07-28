afa87f25cad958ec5f370c1dcaef8bae06c38ecc
Eunchong Kim <chariskimec@gmail.com>
2021-07-28 11:04:01 +0900

    Cleanup

--- i2c.py
+++ i2c.py
@@ -1,22 +1,34 @@
-# https://pypi.org/project/smbus2/
-import smbus2
+#!/bin/env python3
+# i2c.py
+#=================================
+# Author: Eunchong Kim
+#=================================
+
 
 import time
 
+# https://pypi.org/project/smbus2/
+import smbus2
+
+
+#=================================
+# Static variables
 # About device
 DEVICE_BUS = 0
 DEVICE_ADDRESS = 0x38
 
 # commands
-INIT_CMD    = 0b1110_0001
-TRIGGER_MEAS = 0b1010_1100 # Trigger measurement
-SOFTRESET = 0b1011_1010
-DATA0 = 0b0011_0011
-DATA1 = 0b0000_0000
+INIT_CMD        = 0b1110_0001 # Initialization command
+TRIGGER_MEAS    = 0b1010_1100 # Trigger measurement. need to wait at least 75 ms
+SOFTRESET       = 0b1011_1010 # Restart the sensor system. need to wait at least 20 ms
+DATA0           = 0b0011_0011
+DATA1           = 0b0000_0000
 
 
+#=================================
 # Create bus
 bus = smbus2.SMBus(DEVICE_BUS)
+
 
 # Initialize
 bus.write_byte_data(
@@ -25,9 +37,11 @@ bus.write_byte_data(
         INIT_CMD
         )
 
-time.sleep(.5) # .5s
+time.sleep(.1) # .1s
 
-# Write to trigger measurement
+
+#=================================
+# Loop
 write_data = [
         TRIGGER_MEAS,
         DATA0,
@@ -35,19 +49,23 @@ write_data = [
         ]
 
 while (True):
+    # Write trigger measurement
     bus.write_i2c_block_data(
             DEVICE_ADDRESS,
             0x00, # Read R : '1' , write W : '0'
             write_data)
 
+    # Sleep at least 75 ms
     time.sleep(1) # 1s
 
+    # Read 6 bytes of data
     read_data = bus.read_i2c_block_data(
             DEVICE_ADDRESS,
             0x01, # Read R : '1' , write W : '0'
             6 # 6 bytes
             )
 
+    # Treat status code
     status = read_data[0]
 
     # Fill data
