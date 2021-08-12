ebc6c44ef7d9f8fe67e4b6258415e49702cc2ba2
Eunchong Kim <chariskimec@gmail.com>
2021-08-12 16:48:07 +0900

    Cleanup

--- i2c.py
+++ i2c.py
@@ -59,14 +59,18 @@ bus.write_byte_data(
 time.sleep(.1) # .1 s
 
 # For CCS811
+# Start application mode
 bus.write_byte_data(
         CCS811_ADDR,
         0, # write
-        CCS811_APP_START_ADDR)
+        CCS811_APP_START_ADDR
+        )
+# Set measurement mode
 bus.write_i2c_block_data(
         CCS811_ADDR,
         0, # write
-        [CCS811_MEAS_MODE_ADDR, CCS811_MEAS_MODE_CMD])
+        [CCS811_MEAS_MODE_ADDR, CCS811_MEAS_MODE_CMD]
+        )
 
 #================================================
 # Loop
@@ -114,23 +118,12 @@ while (True):
 
     # CCS811, read total 8 bytes of data
     # Select result register address
-    read_data = i2c_msg.read(CCS811_ADDR, 8) # 8 bytes
+    i2c_read = i2c_msg.read(CCS811_ADDR, 8) # 8 bytes
     bus.i2c_rdwr(
             i2c_msg.write(CCS811_ADDR, [CCS811_RESULT_ADDR]),
-            read_data
+            i2c_read
             )
-    read_data = [ int(data) for data in read_data ]
-    # Convert i2c_msg to integer
-    #bus.write_byte_data(
-    #        CCS811_ADDR,
-    #        0, # write
-    #        CCS811_RESULT_ADDR
-    #        )
-    #read_data = bus.read_i2c_block_data(
-    #        CCS811_ADDR,
-    #        1, # read
-    #        8 # 8 bytes
-    #        )
+    read_data = list(i2c_read) # Convert i2c_msg to integer
 
     # Fill data
     eCO2 = (read_data[0] << 8) + read_data[1] # equivalent CO2. from 400 ppm to 8192 ppm
@@ -141,8 +134,9 @@ while (True):
     raw_adc = (read_data[6] & 0b0000_0011) + read_data[7]
 
     # Treat status
-    if not status & 2**0 == 0:
-        print('Error')
+    #if not status & 2**0 == 0:
+    if not status == 0x98:
+        print('CCS811 Status Warning')
     else:
         print('{0}, eCO2: {1} ppm, TVOC: {2} ppb'.format(current_datetime_str, eCO2, TVOC))
 
