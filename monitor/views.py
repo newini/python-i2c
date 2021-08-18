7767161acb73b4aa4e33261d51518a7c414b09ea
Eunchong Kim <chariskimec@gmail.com>
2021-08-19 00:26:40 +0900

    Added templates

--- monitor/views.py
+++ monitor/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Data
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    data_list = Data.objects.all()
+    context = {
+            'data_list': data_list
+            }
+    return render(request, 'index.html', context)
