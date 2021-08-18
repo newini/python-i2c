bdc006c1bb633b767b6d2ef3b43d3ac3b1a6888e
Eunchong Kim <chariskimec@gmail.com>
2021-08-18 22:59:24 +0900

    Added index, added data model

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Data(models.Model):
+    """
+    Data with huminity, temperature, eCO2, TVOC
+    """
+
+    # Fields
+    create_at = models.DateTimeField(help_text='Created Datetime')
+    huminity = models.FloatField(help_text='Huminity')
+    temperature = models.FloatField(help_text='Temperature')
+    eCO2 = models.IntegerField(help_text='eCO2')
+    TVOC = models.IntegerField(help_text='TVOC')
+
+    ## Metadata
+    #class Meta:
+    #    ordering = ['-my_field_name']
+
+    ## Methods
+    #def get_absolute_url(self):
+    #    """Returns the url to access a particular instance of MyModelName."""
+    #    return reverse('model-detail-view', args=[str(self.id)])
+
+    #def __str__(self):
+    #    """String for representing the MyModelName object (in Admin site etc.)."""
+    #    return self.my_field_name
